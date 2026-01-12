1fcd851306d8e93f5ce7d2150c3e9837b16df81e
rishiakrishnan <rishiakrishnan@gmail.com>
2026-01-12 19:07:30 +0530

    changes all

--- new-main.sh
+++ new-main.sh
@@ -1,2 +1,8 @@
+<<<<<<< HEAD
 # feature from the main branch
 hello this is from main
+=======
+# feature from the main branch ok bro the changes have be done
+ok bro thank you
+sample change 
+>>>>>>> additional-feature
